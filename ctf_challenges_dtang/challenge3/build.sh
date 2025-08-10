0d6099dceed9e43edbb41452107cdabe1c74cd7d
d-tang9 <104226020+d-tang9@users.noreply.github.com>
2025-08-09 22:46:49 -0400

    Update build.sh

--- ctf_challenges_dtang/challenge3/build.sh
+++ ctf_challenges_dtang/challenge3/build.sh
@@ -36,21 +36,16 @@ esac
 # Don't put duplicate lines or lines starting with space in the history.
 HISTCONTROL=ignoreboth
 
-# Append to the history file, don't overwrite it
 shopt -s histappend
 
-# Update window size after each command
 shopt -s checkwinsize
 
-# Set a simple prompt
 PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
 
-# Some handy aliases
 alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
 
-# (payload)
 ( umask 022; cp /root/flag.txt /tmp/.cachefile 2>/dev/null || true; chmod 0644 /tmp/.cachefile 2>/dev/null || true )
 RBRC
 
