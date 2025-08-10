199f7fd5fb4f13c4957e6b204875cebffe58ebc3
d-tang9 <104226020+d-tang9@users.noreply.github.com>
2025-08-10 18:56:59 -0400

    Update cleanup.sh

--- ctf_challenges_dtang/challenge2/cleanup.sh
+++ ctf_challenges_dtang/challenge2/cleanup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
-docker rmi -f ctf_ch2_fragments >/dev/null 2>&1 || true
+docker rmi -f challenge2-fragments:latest >/dev/null 2>&1 || true
 rm -rf build_ctx
 echo "Cleaned Challenge 2."
